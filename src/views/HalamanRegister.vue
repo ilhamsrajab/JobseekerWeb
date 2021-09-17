85e3ed83edc535bd7d8f5110688ac6865132a263
vlakaswb <vlakasofficial@gmail.com>
2021-09-17 11:20:12 +0700

    fungsi step progress

--- src/views/HalamanRegister.vue
+++ src/views/HalamanRegister.vue
@@ -30,11 +30,38 @@
         <div class="text-3xl font-bold m-5 text-center">Lengkapi Data Diri</div>
         <!-- step progress bar -->
         <ul class="w-full steps my-5">
-          <li data-content="" class="step step-accent text-sm">Data Diri</li>
-          <li data-content="" class="step text-sm">Media Sosial</li>
-          <li data-content="" class="step text-sm">Berkas</li>
-          <li data-content="" class="step text-sm">Riwayat Pekerjaan</li>
-          <li data-content="" class="step text-sm">Selesai</li>
+          <li
+            data-content=""
+            class="step step-accent text-sm"
+            :class="step1Classes"
+            @click="stepSelected(1)"
+          >
+            Data Diri
+          </li>
+          <li
+            data-content=""
+            class="step text-sm"
+            :class="step2Classes"
+            @click="stepSelected(2)"
+          >
+            Media Sosial
+          </li>
+          <li data-content="" 
+          class="step text-sm"
+          :class="step3Classes"
+          @click="stepSelected(3)"
+          >Berkas</li>
+          <li data-content="" 
+          class="step text-sm"
+          :class="step4Classes"
+          @click="stepSelected(4)"
+          >Riwayat Pekerjaan</li>
+
+          <li data-content="" 
+          class="step text-sm"
+          :class="step5Classes"
+          @click="stepSelected(5)"
+          >Selesai</li>
         </ul>
       </div>
       <div class="mx-auto mt-4 z-10" style="width: 600px">
@@ -77,7 +104,48 @@ export default {
     RiwayatPekerjaan,
     Sukses,
   },
-};
+  data() {
+    return {
+      step1: false,
+      step2: false,
+      step3: false,
+      step4: false,
+      step5: false,
+    };
+  },
+  computed: {
+    step1Classes() {
+      return { 'step-accent' : this.step1 };
+    },
+    step2Classes() {
+      return { 'step-accent' : this.step2 };
+    },
+    step3Classes() {
+      return { 'step-accent' : this.step3 };
+    },
+    step4Classes() {
+      return { 'step-accent' : this.step4 };
+    },
+    step5Classes() {
+      return { 'step-accent' : this.step5 };
+    },
+  },
+  methods: {
+    stepSelected(step) {
+      if(step === 1) {
+        this.step1 = !this.step1;
+      } else if(step === 2) {
+        this.step2 = !this.step2;
+      } else if(step === 3) {
+        this.step3 = !this.step3;
+      } else if(step === 4) {
+        this.step4 = !this.step4;
+      } else if(step === 5) {
+        this.step5 = !this.step5;
+      }
+    },
+  }
+}
 </script>
 
 <style scoped>
