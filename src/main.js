1aba45f63674ca0f1b3ec9be48a95e11138cb742
VlakasWB <vlakasofficial@gmail.com>
2021-09-23 07:32:54 +0700

    add vuex

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@
 import { createApp } from 'vue/dist/vue.esm-bundler';
 import App from './App.vue'
 import router from './router/'
+import store from './store/index.js';
 
 import './assets/tailwind.css'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(router);
+app.use(store);
+
+app.mount('#app');
 // createApp(App).mount('#app')
