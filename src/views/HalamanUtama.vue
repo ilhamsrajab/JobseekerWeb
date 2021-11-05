96406172be00a4abffc3dab10bc67f6639b05fd5
ilhamsrajab <iamidat@outlook.com>
2021-11-05 11:26:19 +0700

    Add required on data diri register

---
+++ src/views/HalamanUtama.vue
@@ -0,0 +1,75 @@
+<template>
+  <!-- <Loading /> -->
+  <div class="drawer drawer-mobile h-screen bg-gray-100 dark:bg-gray-800">
+    <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+
+    <div class="drawer-content flex-1 flex flex-col overflow-hidden">
+      <!-- header -->
+      <Header />
+
+      <!-- main page -->
+      <main class="flex flex-col h-screen overflow-x-hidden overflow-y-auto">
+        <HalamanHome />
+        <!-- <HalamanCariKerja /> -->
+        <!-- <Footer /> -->
+      </main>
+      <!-- end of main page  -->
+    </div>
+    <!-- sidebar -->
+    <div class="drawer-side">
+      <label for="my-drawer-2" class="drawer-overlay"></label>
+      <Sidebar />
+    </div>
+    <!-- end of sidebar -->
+  </div>
+</template>
+
+<script>
+import Loading from "../components/UI/Loading.vue";
+import Sidebar from "../components/Layout/Sidebar.vue";
+import Header from "../components/Layout/Header.vue";
+import Footer from "../components/Layout/Footer.vue";
+import HalamanHome from "../views/HalamanHome.vue";
+import HalamanCariKerja from "../views/HalamanCariKerja.vue";
+import { Icon } from "@iconify/vue";
+
+export default {
+  components: {
+    Loading,
+    Sidebar,
+    Header,
+    Footer,
+    HalamanHome,
+    HalamanCariKerja,
+    Icon,
+  },
+};
+</script>
+
+<style scoped>
+/* Hide scrollbar for Chrome, Safari and Opera */
+.no-scrollbar::-webkit-scrollbar {
+  display: none;
+}
+
+/* Hide scrollbar for IE, Edge and Firefox */
+.no-scrollbar {
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+}
+
+#email {
+  display: block;
+  width: 11.5rem;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+
+#notif {
+  display: block;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+</style>
